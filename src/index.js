7217564eb78b438d550d6c2016703657ad5f44a0
SiarheiKresik <siarhei.kresik@gmail.com>
2018-03-13 09:46:12 +0300

    Implementing solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var a, b, c; // coefficients
+  [a, b, c] = parseEquation(equation);
+  var d = b * b - 4 * a * c; // discriminant
+  var x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+  var x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+  return [x1, x2].sort((x, y) => x - y);
+}
+
+function parseEquation(equation) {
+  // Returns an array of coefficients.
+  return equation.replace(/\s+/g, '')
+    .split(/\*x(?:\^2)?/)
+    .map(Number);
 }
